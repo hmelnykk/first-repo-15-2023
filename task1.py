7ceffb84cbdf7ca4bbe3f165ac04ce6437596b46
hmelnykk <151665742+hmelnykk@users.noreply.github.com>
2023-11-22 14:03:01 +0200

    added code of my first lab code (#3)

---
+++ task1.py
@@ -0,0 +1,20 @@
+drink = "чай"
+print(f"Крутий напій {drink}")
+
+is_holiday = True
+is_workday = False
+
+if is_holiday:
+    print("Вихідний")
+else:
+    print("Не вихідний")
+
+if is_workday:
+    print("Робочий день")
+else:
+    print("Не робочий день")
+
+is_sunny = True
+is_cloudy = True
+
+print(is_sunny or is_cloudy)
